948838069e3e4d7a2662e899262a8b63b9f2e65b
Marearcat <marearcat@gmail.com>
2020-09-15 07:36:06 +0300

    Добавьте файлы проекта.

---
+++ SSInternship_Latfullin/Hubs/GeneralData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSInternship_Latfullin.Hubs
+{
+    public class GeneralData
+    {
+        public GeneralData()
+        {
+            History = new List<string>();
+        }
+
+        public List<string> History { get; set; }
+    }
+}
